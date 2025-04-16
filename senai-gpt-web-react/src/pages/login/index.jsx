8855821eb06f2b4323052ae33a71445b4075b8ba
Vinicius <vinicius2145.god@gmail.com>
2025-04-15 21:37:38 -0300

    salvando

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -1,12 +1,31 @@
 import "./login.css"
 
-function Login(){ 
+function Login() {
 
-    return (
-      <>
-      <h1>Login</h1>
-      </>
-    )
-  }
-  export default Login
-  
+  return (
+    <>
+      <main className="paige-contener">
+        <div className="roboimage"></div>
+
+        <div className="logincontener">
+
+          <img className="logo" src="../assents/imgs/Chat.png" alt="logo do senai GPT" />
+
+          <h1
+          
+            id="meutitulo"
+            className="titulo"
+          >Login</h1>
+
+          <input className="inpt" type="email" placeholder="Insira o e-mail" />
+          <input className="inpt" type="password" placeholder="Insira a senha" />
+
+          <button className="btn">Entra</button>
+        </div>
+
+
+      </main>
+    </>
+  )
+}
+export default Login
